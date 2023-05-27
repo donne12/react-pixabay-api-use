8bc40bf952ed61242713298a962c6be93a1e980b
donne12 <donnesiaku@yahoo.com>
2023-05-27 16:24:43 +0200

    tailwind-css setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import API from './API';
-import Image from './Image';
-import Masonry from 'react-masonry-css';
-import InfiniteScroll from 'react-infinite-scroller';
+import React, { useEffect, useState } from "react";
+import API from "./API";
+import Image from "./Image";
+import Masonry from "react-masonry-css";
 
 let pageNum = 1;
 
 const App = () => {
   const [imagesArray, setImagesArray] = useState([]);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const fetchImages = (pageNumber, query) => {
-    API.get('/', { params: { page: pageNumber, q: query } })
+    API.get("/", { params: { page: pageNumber, q: query } })
       .then((res) => {
-        console.log(res.data);
         setImagesArray([...res.data.hits]);
       })
       .catch((err) => console.log(err));
@@ -28,7 +26,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchImages(pageNum, '');
+    fetchImages(pageNum, "tech");
   }, []);
 
   const breakpointColumnsObj = {
@@ -41,8 +39,13 @@ const App = () => {
 
   return (
     <div className="App">
+
       <div className="container-fluid">
-        <h1 className="text-center text-4xl font-bold my-4">Galerie d'images</h1>
+        <h1 className="text-center text-4xl font-bold my-4">
+          Galerie d'images
+        </h1>
+
+      
         <div className="flex items-center justify-center">
           <input
             className="py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -51,25 +54,25 @@ const App = () => {
             onChange={handleInputChange}
             placeholder="Trouver une image..."
           />
-          <button onClick={handleSearchClick} className="ml-2 px-4 py-2 bg-blue-500 text-black rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <button
+            onClick={handleSearchClick}
+            className="ml-2 px-4 py-2 bg-blue-500 text-black rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
             Rechercher
           </button>
         </div>
+
         <div className="row">
           <div className="col-md-12">
-            <InfiniteScroll
- 
+            <Masonry
+              breakpointCols={breakpointColumnsObj}
+              className="masonry-grid"
+              columnClassName="masonry-grid_column"
             >
-              <Masonry
-                breakpointCols={breakpointColumnsObj}
-                className="masonry-grid"
-                columnClassName="masonry-grid_column"
-              >
-                {imagesArray.map((image) => (
-                  <Image key={image.id} {...image} />
-                ))}
-              </Masonry>
-            </InfiniteScroll>
+              {imagesArray.map((image) => (
+                <Image key={image.id} {...image} />
+              ))}
+            </Masonry>
           </div>
         </div>
       </div>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ], purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
